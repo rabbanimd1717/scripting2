a04ba47689af970c29a8b2ddf4ac8c684e866723
rabbanimdr <rabbani@gmail.com>
2025-06-02 17:28:12 +0530

    practice of shell scripting

--- 1.practice.sh
+++ 1.practice.sh
@@ -13,3 +13,17 @@ echo "$person2 is a billionarie"
 
 echo "$person1 and $person2 is same persons"
 
+#command line argument
+
+person1=$1
+script_name=$0
+PERSON2=$2
+
+echo "$person1 is a devops engineer"
+
+echo "$PERSON2 is a billionarie"
+
+echo "$person1 and $PERSON2 is same persons"
+
+
+
