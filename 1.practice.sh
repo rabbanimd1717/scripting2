70d3be75f5030f2690e89dc3d6f1ba0186566c83
rabbanimdr <rabbani@gmail.com>
2025-06-02 17:24:01 +0530

    practice of shell scripting

--- 1.practice.sh
+++ 1.practice.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 echo "Sample code with practice"
+
+#variables
+
+person1=Rabbani
+person2=MDR
+
+echo "$person1 is a devops engineer"
+
+echo "$person2 is a billionarie"
+
+echo "$person1 and $person2 is same persons"
+
