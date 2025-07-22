80f585bb56624582666171822ce7a48f023dcfdb
rabbani <rabbanimohammed@gmail.com>
2025-07-22 10:17:11 +0530

    hello worl project written by rabbani

--- 06.readvariable3.sh
+++ 06.readvariable3.sh
@@ -2,8 +2,7 @@
 
 #using -p option for prompt 
 
-echo "Enter your age without date of birth: " age
+read -s -p "Enter your age without date of birth: " age
 
-read -s -p age 
 
 echo "correct age is $age"
