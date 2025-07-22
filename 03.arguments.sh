0c11fac7212808a8bc9acc01260cbbff016ac36e
rabbani <rabbanimohammed@gmail.com>
2025-07-22 09:54:13 +0530

    hello worl project written by rabbani

---
+++ 03.arguments.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#command line arguments is while running the scripts, if you are passing any values called as command line interface.
+
+PERSON1=$1
+PERSON2=$2
+
+echo $0
+
+echo "$PERSON1: Hi $PERSON2 i'm $PERSON1 How are you"
+echo "$PERSON2: im fine nice to meet you. what about you $PERSON1"
+echo "$PERSON1: im also good $PERSON2. I'm a big fan of to you $PERSON2."
+echo "$PERSON2: Thanks $PERSON1 its mean a lot to me."
+echo "$PERSON1: im big fan of that inningd id 25 balls of 87 runs you scored $PERSON2"
