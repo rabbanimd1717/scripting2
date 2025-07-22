7b184cda2adcf3fb9587bbff95ec042401ba0838
rabbani <rabbanimohammed@gmail.com>
2025-07-22 23:05:25 +0530

    hello worl project written by rabbani

--- 11.installation.sh
+++ 11.installation.sh
@@ -11,12 +11,12 @@ then
     echo "This is super user proceed to run the package"
 else
     echo "This is not sudo user not proceeding further"
-    exit 1
+    exit 1 #manually ecit if error comes
 fi
 
-dnf install gitt -y
+dnf install git -y
 
-if [ $? -eq 0 ]
+if [ $? -eq 0 ] #$? to check/status of previous exit command if success shown 0 if failure shown 1 to 127
 then
     echo "Git installtion is SUCCESS"
 else
