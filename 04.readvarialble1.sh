4573e93003d850d376d62921828319d448462c7f
rabbani <rabbanimohammed@gmail.com>
2025-07-22 09:59:37 +0530

    hello worl project written by rabbani

---
+++ 04.readvarialble1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Read a value at runtime in shell scripting
+
+echo "login to facebook: "
+
+read username
+
+echo "enter your password: "
+
+read password
+
+echo "enter your credentials: username is: $username, and password is: $password"
