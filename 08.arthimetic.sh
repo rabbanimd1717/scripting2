3de51c58b798972ef2d4dbaae2df495201e2a39c
rabbani <rabbanimohammed@gmail.com>
2025-07-22 17:24:39 +0530

    hello worl project written by rabbani

---
+++ 08.arthimetic.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Arthmetic operations
+
+MYSALARY=$1
+EXPENSES=$2
+
+echo $0
+echo $1
+echo $2
+
+echo "sum of addition is $((MYSALARY+EXPENSES))
+echo "sub is: $((MYSALARY-EXPENSES))"
