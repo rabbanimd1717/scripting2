767ee84ceca51298c42502a772ff7113737519b8
rabbani <rabbanimohammed@gmail.com>
2025-07-22 09:44:47 +0530

    hello worl project written by rabbani

--- 02-variable.sh
+++ 02-variable.sh
@@ -4,7 +4,7 @@ PERSON1=Pawan
 PERSON2=MAhesh
 
 echo "$PERSON1: Hi $PERSON2 i'm $PERSON1 How are you"
-echo  $PERSON2: im fine nice to meet you. what about you $PERSON1"
+echo  "$PERSON2: im fine nice to meet you. what about you $PERSON1"
 echo "$PERSON1: im also good $PERSON2. I'm a big fan of to you $PERSON2."
 echo  $PERSON2: Thanks $PERSON1 its mean a lot to me."
 echo "$PERSON1: im big fan of that inningd id 25 balls of 87 runs you scored $PERSON2"
