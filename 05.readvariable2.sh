d6b9a411997857d79fc4d23a6c75987033502126
rabbani <rabbanimohammed@gmail.com>
2025-07-22 10:11:40 +0530

    hello worl project written by rabbani

---
+++ 05.readvariable2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#command line arguments with read a value at runtime with silently
+
+echo $shell
+
+echo "Enter instagram username: "
+
+read -s USERNAME
+
+echo "Enter Instagram password: "
+
+read -s PASSWORD
+
+echo "$USERNAME and $PASSWORD"
