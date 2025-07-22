d787ee16d059b4888ce38647cdc921d7c0f5982c
rabbani <rabbanimohammed@gmail.com>
2025-07-22 09:28:41 +0530

    hello worl project written by rabbani

---
+++ 02-variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Rabbani: Hi Raina i'm Rabbani How are you"
+echo "Raina: im fine nice to meet you. what about you Rabbani"
+echo "Rabbani: im also good Raina. I'm a big fan of to you Raina."
+echo "Raina: Thanks Rabbani its mean a lot to me."
+echo "Rabbani: im big fan of that inningd id 25 balls of 87 runs you scored Raina"
