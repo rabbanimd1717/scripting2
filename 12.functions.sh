a5ad1aa015a5c9823131be100570b3b21d0c8e96
rabbani <rabbanimohammed@gmail.com>
2025-07-25 11:04:06 +0530

    hello worl project written by rabbani

--- 12.functions.sh
+++ 12.functions.sh
@@ -4,3 +4,11 @@
 
 #block of code is repeating, considering function
 
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ]
+then
+    echo "This is not super user....dont go to further"
+else
+    echo "This is SUPER USER then install packages"
+fi
