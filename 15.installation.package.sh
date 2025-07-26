323c49387495fd01fb6d2a7d5a2e7443830f8a48
rabbani <rabbanimohammed@gmail.com>
2025-07-26 11:57:47 +0530

    hello worl project written by rabbani

---
+++ 15.installation.package.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+USER_ID=(id -u)
+
+
+if [ USER_ID -eq 0 ]
+then
+    echo "This is super user...proceed to install the packages"
+else
+    echo "This not a super user....dont go to further"
+fi
+
