64d07ded3c0d465e3396a06cc0d0057eb653727f
rabbani <rabbanimohammed@gmail.com>
2025-07-26 12:11:47 +0530

    hello worl project written by rabbani

--- 15.installation.package.sh
+++ 15.installation.package.sh
@@ -2,11 +2,26 @@
 
 USER_ID=$(id -u)
 
+VALIDATE_FUN(){
+    if [ $1 -eq 0 ]
+    then
+        echo "$2 is SUCCESS"
+    else
+        echo "$2 is FAILURE"
+    fi
+}
+
 
 if [ $USER_ID -eq 0 ]
 then
     echo "This is super user...proceed to install the packages"
 else
     echo "This not a super user....dont go to further"
+    exit 1
 fi
 
+dnf install mysql -y
+
+
+
+VALIDATE_FUN $? "INSTALLING MYSQL"
